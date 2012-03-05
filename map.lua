50cb504cec16488fdb76c5443a1be32cfb974175
Alex Shubert <spamme@not.ru>
2012-03-05 20:53:38 +0400

    antispam moved to standalone addone

--- map.lua
+++ map.lua
@@ -1,12 +1,10 @@
 --[[ 	
 	Adds player's coordinates to the map
 ]]--
-
-local coord = WorldMapFrame:CreateFontString()
+local coord = WorldMapFrame:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
 coord:SetPoint("BOTTOMRIGHT", WorldMapPositioningGuide, "BOTTOMRIGHT", -400, 11)
 coord:SetHeight(10)
 coord:SetWidth(400)
-coord:SetFont("Fonts\\FRIZQT__.TTF", 12, "OUTLINE, MONOCHROME")
 
 local function GetMouseCoord()
 	local scale = WorldMapDetailFrame:GetEffectiveScale()
@@ -37,9 +35,7 @@ end
 WorldMapFrame:HookScript("OnUpdate", function(self, button)
         local px, py = roundCoords(GetPlayerMapPosition("player"))
 		local mx, my = GetMouseCoord()
-        mx = mx > 0 and string.format("%02.1f", mx) or "00.0"
-        my = my > 0 and  string.format("%02.1f", my)  or "00.0"
-        local playerCoords = string.format("you = %02.1f / %02.1f mouse = %s / %s", px, py, mx, my)
+        local playerCoords = string.format("mouse = %04.1f / %04.1f you = %04.1f / %04.1f", mx, my,px, py)
 		coord:SetText(playerCoords)
     end)	
 

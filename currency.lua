0a6e456e88b57ad1b249cdf66856932cb01a5a37
unknown <alex@shubert.ru>
2014-11-24 18:51:52 +0300

    Missing lua added

---
+++ currency.lua
@@ -0,0 +1,42 @@
+--[[ 
+* adds timestamp
+* shortens channel names
+* shows popup windows by right-click on timestamp in order to copy chat string
+* replace url most of them) with link, which shows popup window on click
+* P.S.: thanks to Borlox, who proved my guesses 
+]]--
+
+LurUI.currency = {
+	frameLoaded = false
+}
+local ui = LurUI.currency
+
+local function CreateUIString(x,y, text)
+	local label = GarrisonLandingPage:CreateFontString(nil, "BORDER", "GameFontNormal")
+	label:SetPoint("TOPLEFT", GarrisonLandingPage, "TOPLEFT", x, y)
+	label:SetJustifyH("RIGHT")
+	label:SetText(text)
+	return label
+end
+
+local function CreateResTexture()
+	local tex = GarrisonLandingPage:CreateTexture(nil, "BORDER")
+	tex:SetSize(12,12)	
+	tex:SetTexture(select(3, GetCurrencyInfo(GARRISON_CURRENCY)));
+	return tex
+end
+
+hooksecurefunc("Garrison_LoadUI", function()
+	if (not ui.frameLoaded) then
+		ui.frameLoaded = true
+		ui.resLabel = CreateUIString(80, -15, "0/0")
+		ui.resIcon = CreateResTexture()
+		ui.resIcon:SetPoint("LEFT", ui.resLabel, "RIGHT", 5, 0)
+		
+		
+		GarrisonLandingPage:HookScript("OnUpdate", function(self, button)
+			local _, CurrentAmount, _, _, _, totalMax, _ = GetCurrencyInfo(GARRISON_CURRENCY)
+			ui.resLabel:SetText(CurrentAmount.."/"..totalMax)
+		end)
+	end
+end)
